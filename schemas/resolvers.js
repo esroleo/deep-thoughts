d81bffe0df5347bace623f6926973c138b7f12d5
esroleo <esroleo@gmail.com>
2021-04-26 12:51:30 -0400

    queries, types and resolves set up

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,8 +1,32 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+        // find all thoughts by username if any provided
+        // if not username provided, give me all thoughts
+        thoughts: async (parent, { username }) => {
+            // ternary operator, if you find use pass the username as params
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+          },
+        // find thought by ID 
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
     }
   };
   
