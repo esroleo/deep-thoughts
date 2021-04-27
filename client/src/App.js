817e2815fa30c5c7b20b9a01c050cd27d45dd1f2
esroleo <esroleo@gmail.com>
2021-04-27 14:46:04 -0400

    react is now talking to graphql with apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
+// Apollo Provider imports - Apollo-boost 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+
+// Connection to graphQL server using apollo
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
 export default App;
+// Esteban Romero
