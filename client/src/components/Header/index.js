2409c30b2ae9999292f4b1adbd8129c89e2b0602
esroleo <esroleo@gmail.com>
2021-04-28 11:07:01 -0400

    user profile page rendering properly

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 // Link will change the URL while staying on the same page
 import { Link } from 'react-router-dom';
 
+import Auth from '../../utils/auth';
+
+
+
 // Use of react links in order to say on the page 
 // while "moving URL" as to avoid talking to the server side
 const Header = () => {
+
+  // logout function 
+  const logout = event => {
+    // this will stop the a element from refreshing the page
+    event.preventDefault();
+    Auth.logout();
+  };
+
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -13,8 +26,19 @@ const Header = () => {
         </Link>
 
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 // user the URL ID values
-import { useParams } from 'react-router-dom';
+// redirect will allow us to redirect the user to another route
+// within the application
+import { Redirect, useParams } from 'react-router-dom';
 // import component thought list
 import ThoughtList from '../components/ThoughtList';
 // import the Friendslist
@@ -9,27 +11,54 @@ import FriendList from '../components/FriendList';
 // allow talking to graphql via apollo
 import { useQuery } from '@apollo/react-hooks';
 // allow our queries to run against apollo
-import { QUERY_USER } from '../utils/queries';
+// query_me will be used to fix issue with link to me on header
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+// fix issue with logged goes to /profile/username and the value
+// of the username is the same as the logged-in user?
+import Auth from '../utils/auth';
+
 
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  // before fix for me url to show /profile/username/
+  // const { loading, data } = useQuery(QUERY_USER, {
+  //   variables: { username: userParam }
+  // });
+  // not sure how this works, query me uses param vs query users does not.
+  // if there is a token query user will fail?
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  // check if the data return is from a logged user or a non logged user
+  const user = data?.me || data?.user || {};
+
+  // redirect to personal profile page if username is the logged-in user's
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
 
   if (loading) {
     return <div>Loading...</div>;
+  }
+
+  // if user goes to profile and is not logged in display this message
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
   }
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
