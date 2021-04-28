fe298dc10f2de42dda7770a50c2ee6bc627c309b
esroleo <esroleo@gmail.com>
2021-04-27 21:46:56 -0400

    react router implemented, pages showing correctly

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 // Apollo Provider imports - Apollo-boost 
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+// import react router for pages feeling to our SPA app.
+// switch is used for our catch all route (missing or not used)
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// our page components imports
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 
 
@@ -21,16 +29,24 @@ const client = new ApolloClient({
   uri: '/graphql'
 });
 
-function App() {
+function App() { // our render for our index.js react script
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/thought" component={SingleThought} />
+
+            <Route component={NoMatch} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
